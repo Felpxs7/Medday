2a4ea240d9f58f0bc3e2dba72b0044f83bac26db
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-18 18:33:34 -0300

    feat: paciente Response e paciente Service

--- medday-api/src/main/java/com/medday/medday_api/Dto/Response/PacienteDtoResponse.java
+++ medday-api/src/main/java/com/medday/medday_api/Dto/Response/PacienteDtoResponse.java
@@ -1,4 +1,36 @@
 package com.medday.medday_api.Dto.Response;
 
+import com.medday.medday_api.Domain.Medico;
+import com.medday.medday_api.Domain.Paciente;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PacienteDtoResponse {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private String email;
+
+    public static PacienteDtoResponse fromEntity(Paciente paciente) {
+        return PacienteDtoResponse.builder()
+                .id(paciente.getId())
+                .nome(paciente.getNome())
+                .cpf(paciente.getCpf())
+                .dataNascimento(paciente.getDataNascimento())
+                .telefone(paciente.getTelefone())
+                .email(paciente.getEmail())
+                .build();
+    }
 }
