b7c8aa411ced35d356e182ba3fddc5bb71044bc9
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-18 21:42:10 -0300

    feat: backend refatorado

--- medday-api/src/main/java/com/medday/medday_api/Repository/MedicoRepository.java
+++ medday-api/src/main/java/com/medday/medday_api/Repository/MedicoRepository.java
@@ -4,5 +4,4 @@ import com.medday.medday_api.Domain.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
-    boolean existsByCrm (String crm);
 }
